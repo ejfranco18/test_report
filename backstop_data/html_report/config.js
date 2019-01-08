3a428998d3c0b967736590a4a25d111907aed010
Javier Franco <javierfranco@google.com>
2019-01-08 15:47:46 -0500

    Project start

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,356 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_0_document_0_phone.png",
+        "label": "about",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.13",
+          "analysisTime": 121
+        },
+        "diffImage": "../bitmaps_test/20190108-154054/failed_diff_sampleproject_about_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_0_document_1_tablet.png",
+        "label": "about",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_0_document_2_desktop.png",
+        "label": "about",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 296
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_where-to-use_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_where-to-use_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_where-to-use_0_document_0_phone.png",
+        "label": "about/where-to-use",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/where-to-use",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_where-to-use_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_where-to-use_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_where-to-use_0_document_1_tablet.png",
+        "label": "about/where-to-use",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/where-to-use",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 148
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_where-to-use_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_where-to-use_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_where-to-use_0_document_2_desktop.png",
+        "label": "about/where-to-use",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/where-to-use",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 157
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_banks_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_banks_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_banks_0_document_0_phone.png",
+        "label": "about/banks",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/banks",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_banks_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_banks_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_banks_0_document_1_tablet.png",
+        "label": "about/banks",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/banks",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_banks_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_banks_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_banks_0_document_2_desktop.png",
+        "label": "about/banks",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/banks",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_learn_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_learn_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_learn_0_document_0_phone.png",
+        "label": "about/learn",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/learn",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_learn_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_learn_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_learn_0_document_1_tablet.png",
+        "label": "about/learn",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/learn",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_learn_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_learn_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_learn_0_document_2_desktop.png",
+        "label": "about/learn",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/learn",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_paypal_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_paypal_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_paypal_0_document_0_phone.png",
+        "label": "about/paypal",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/paypal",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_paypal_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_paypal_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_paypal_0_document_1_tablet.png",
+        "label": "about/paypal",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/paypal",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/sampleproject_about_paypal_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190108-154054/sampleproject_about_paypal_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "sampleproject_about_paypal_0_document_2_desktop.png",
+        "label": "about/paypal",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://pay.google.com/about/paypal",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "sample project"
+});
